81e7d144a593dc4804742dcffafd531b134c0164
harel858 <harellevi85@gmail.com>
2022-12-29 22:19:06 +0200

    zzz

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
@@ -12,9 +14,13 @@ const cors_1 = __importDefault(require("cors"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
 app.use(body_parser_1.default.json());
-app.use((0, cors_1.default)({
+app.use(
+  (0, cors_1.default)({
     credentials: true,
     origin: `${process.env.CLIENT}`,
-}));
+  })
+);
 app.use("/", customers_1.default);
-app.listen(process.env.PORT, () => console.log(`app is listening on port ${process.env.PORT}`));
+app.listen(process.env.PORT, () =>
+  console.log(`app is listening on port ${process.env.PORT}`)
+);
