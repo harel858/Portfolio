3c378d6f7654a65ad391b43f1c3250df73284ce6
harel858 <harellevi85@gmail.com>
2022-12-31 17:22:12 +0200

    allowedHeaders

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -13,14 +13,15 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const port = process.env.PORT || 9000;
 const app = (0, express_1.default)();
 app.use(body_parser_1.default.json());
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    next();
-});
+/* app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+}); */
 app.use((0, cors_1.default)({
-    origin: "*",
+    origin: `*`,
+    allowedHeaders: `*`,
 }));
 app.use("/", customers_1.default);
 app.listen(port, () => console.log(`app is listening on port ${process.env.PORT}`));
--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -12,16 +12,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
-});
+}); */
 
 app.use(
   cors({
-    origin: "*",
+    origin: `*`,
+    allowedHeaders: `*`,
   })
 );
 
